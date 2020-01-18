6c7d4014ee16798a77e0185162cb060e067081cf
Karim Ahmed <karim.ahmed@outlook.com>
2020-01-18 20:03:47 +0200

    added changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello!@!!</p>
+          Cjhnaged!@!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
